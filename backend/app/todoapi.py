ef6b9233ff2037a2c5267efed5c122799f584d8b
Lars Martin S. Pedersen <larsmsp@gmail.com>
2017-06-15 13:48:03 +0200

    Added search method

--- backend/app/todoapi.py
+++ backend/app/todoapi.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import os
+
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl import Search
+from flask import Flask, request, jsonify
 from peewee import *
 from playhouse.flask_utils import FlaskDB
 
@@ -9,6 +13,10 @@ DATABASE = {
     'password': "password"
 }
 
+ES_HOSTS_ENV = 'ELASTICSEARCH_HOSTS'
+es_hosts = ['10.0.0.10:9200'] if ES_HOSTS_ENV not in os.environ else str(os.environ[ES_HOSTS_ENV]).split(',')
+
+connections.create_connection(hosts=es_hosts)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -31,9 +39,30 @@ def create():
     pass
 
 
-@app.route("/complete/<int:item_id>", methods=["PUT"])
+@app.route("/complete/<int:item_id>", methods=['PUT'])
 def complete(item_id):
     pass
+
+
+@app.route("/search", methods=['GET'])
+def search():
+    query = request.args['query']
+    results = Search(index='todo')\
+        .highlight_options(require_field_match=False,
+                           fragment_size=255,
+                           pre_tags=['**'],
+                           post_tags=['**'])\
+        .highlight('description')\
+        .query('query_string', query=query).execute()
+    response = []
+    for hit in results:
+        response.append({
+            'id': hit.meta.id,
+            'description': hit.description,
+            'completed': hit.completed,
+            'highlight': list(hit.meta.highlight.contents)
+        })
+    return jsonify(response)
 
 
 def main():
